4acfa0596fdfc8a7bcaffb86fefb5d30d97443ef
Douglas Young <young.d@elsevier.com>
2010-07-08 15:25:44 -0400

    spares and fives passed

--- src/app/Bowling/BowlingGame.cs
+++ src/app/Bowling/BowlingGame.cs
@@ -3,10 +3,18 @@ namespace Bowling
     public class BowlingGame
     {
         int total;
+        private int current;
+        private int last;
 
         public void Throw(int pins)
-        {
+        {            
             total += pins;
+            if (current + last == 10)
+            {
+                total += pins;
+            }
+            last = current;
+            current = pins;
         }
 
         public int GetScore()
