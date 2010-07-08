c9ba0499053234a58c6a70fb339a1591756fd0a9
Douglas Young <young.d@elsevier.com>
2010-07-08 15:36:39 -0400

    spares and fives and twos passed

--- src/app/Bowling/BowlingGame.cs
+++ src/app/Bowling/BowlingGame.cs
@@ -5,11 +5,13 @@ namespace Bowling
         int total;
         private int current;
         private int last;
+        private bool isFirst;
 
         public void Throw(int pins)
-        {            
+        {
+            isFirst = !isFirst;
             total += pins;
-            if (current + last == 10)
+            if (current + last == 10 && isFirst)
             {
                 total += pins;
             }
