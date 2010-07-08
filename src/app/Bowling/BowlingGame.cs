fad36e98487b115a589b011f2d82088f5463df3e
Douglas Young <young.d@elsevier.com>
2010-07-08 15:45:44 -0400

    all fives

--- src/app/Bowling/BowlingGame.cs
+++ src/app/Bowling/BowlingGame.cs
@@ -6,17 +6,21 @@ namespace Bowling
         private int current;
         private int last;
         private bool isFirst;
+        private int totalThrows;
 
         public void Throw(int pins)
         {
+            
             isFirst = !isFirst;
             total += pins;
-            if (current + last == 10 && isFirst)
+            
+            if (current + last == 10 && isFirst && totalThrows < 19)
             {
                 total += pins;
             }
             last = current;
             current = pins;
+            totalThrows ++;
         }
 
         public int GetScore()
